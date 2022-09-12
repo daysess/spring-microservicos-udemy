24bfdfd18fe3e93eacd9249733abe67c6202cd69
daysess <daysedesign@gmail.com>
2022-09-12 16:27:37 -0300

    Primeiro commit

---
+++ hr-worker/src/test/java/com/daysesoares/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daysesoares.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
